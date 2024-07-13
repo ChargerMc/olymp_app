f51ccde4aa7d6c946a0c98fa4c38797cc279e4b4
Nicolas PC <ndarquea95@gmail.com>
2024-07-12 23:38:14 -0500

    ejemplo

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ events = EventService()
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text(f"Estos son los eventos actuales: {events.get_events()}")))
+    page.add(ft.SafeArea(ft.Column(
+        [
+        ft.Text(f"Estos son los eventos actuales: {events.get_events()}"),
+        ft.ElevatedButton("Crear evento", on_click=lambda: events.create_event("Evento")),
+    ])))
 
 
 ft.app(main)
